b126e564dafcabab79e05c0034964b98e2575dbc
YAP WEI JUN <yapweijun1996@gmail.com>
2025-10-24 14:17:57 +0800

    Adjust A4 page width to 720px

--- weditor/weditor.js
+++ weditor/weditor.js
@@ -1,7 +1,7 @@
 (function(){
   "use strict";
   const WCfg=(function(){
-    const A4W=794, A4H=1050, HDR_H=84, FTR_H=64, PAD=0;
+    const A4W=720, A4H=1050, HDR_H=84, FTR_H=64, PAD=0;
     const HDR_MIN=24, FTR_MIN=20;
     const UI={ brand:"#0f6cbd", brandHover:"#0b5aa1", border:"#e1dfdd", borderSubtle:"#c8c6c4", text:"#323130", textDim:"#605e5c", surface:"#ffffff", canvas:"#f3f2f1" };
     const DEBOUNCE_PREVIEW=280, MOBILE_BP=900, PREVIEW_MAX_SCALE=1;
